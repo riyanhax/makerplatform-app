ab260c0081f3efe8972ed5864988bffa3d4b9a78
Egor Makarenko <egormkn@yandex.ru>
2018-07-29 18:09:50 +0500

    up

--- resources/views/addProject.blade.php
+++ resources/views/addProject.blade.php
@@ -1,18 +1,31 @@
-<html>
-	<head>
-		<title></title>
-		<meta charset="utf-8">
-	</head>
-	
-	<body>
-		<img src="images/avatar.jpg">
-		{{$infoUser['surname']}} {{$infoUser['name']}}  {{$infoUser['patronymic']}}
-		<form method='post' action="{{route('saveProject')}}"  enctype="multipart/form-data"> <!--атрибуты у формы не убирать-->
-			Название проекта <input type="text" name="Projectname">
-			<p>Описание проекта <textarea name="Description"></textarea>
-			<p>Загрузите доументы <input type="file" multiple name="file[]">
-			<p><button type=submit>Загрузить</button>
-			{{ csrf_field() }} <!--это тоже не убирать-->
+@extends('layouts.app') @section('content')
+<section id="about">
+	<div class="jumbotron jumbotron-fluid text-white my-0" style="background: url('assets/background.jpg') center center fixed no-repeat; background-size: cover;">
+		<div class="container">
+			<h1 class="display-5">Добавление проекта</h1>
+		</div>
+	</div>
+</section>
+
+<section id="contests" class="py-1">
+	<div class="container my-5">
+		<form method="post" action="{{route('saveProject')}}" enctype="multipart/form-data">
+			<div class="form-group">
+				<label for="exampleFormControlInput1">Название проекта</label>
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""  name="Projectname">
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Описание проекта</label>
+				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="Description"></textarea>
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlSelect1">Файлы проекта</label>
+				<input type="file" class="form-control" multiple name="file[]">
+			</div>
+			<button class="btn btn-primary" type=submit>Загрузить</button>
+			{{ csrf_field() }}
 		</form>
-	</body>
-</html>
+	</div>
+</section>
+
+@endsection
